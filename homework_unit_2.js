3c33b9738feaa54acad78339b6cc22aa639956d1
Joshua Kemp <josh.kemp@ziplist.com>
2014-03-10 20:58:51 -0400

    Learning some basic Javascript

---
+++ homework_unit_2.js
@@ -0,0 +1,12 @@
+
+function fix_machine(debris, product)
+{
+alert("Hey! " + debris + ", " + product);
+}
+
+
+// ### TEST CASES ###
+// print "Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity') == "Give me something that's not useless next time."
+// print "Test case 2: ", fix_machine('buy me dat Unicorn', 'Udacity') == 'Udacity'
+// print "Test case 3: ", fix_machine('AEIOU and sometimes y... c', 'Udacity') == 'Udacity'
+// print fix_machine('wsx0-=mttrhix', 't-shirt')
---
+++ javascript.js
@@ -0,0 +1,4 @@
+function show_image()
+{
+document.getElementById("demo").innerHTML=Date();
+}
