d1122c0ae6e68a51f8046b7bf345314db8c74eb4
Joshua Kemp <josh.kemp@ziplist.com>
2014-03-13 22:00:29 -0400

    Getting a pop up box to work in different location

---
+++ alert.js
@@ -0,0 +1,6 @@
+function popup(pageURL, title, popupWidth, popupHeight)
+    {
+        var left = (screen.width / 2) - (popupWidth / 2);
+        var top = (screen.height / 2) - (popupHeight / 2);
+        var targetPop = window.open("http://barbelle.leoweekly.com/wp-content/uploads/2013/06/scary-monster-copy.jpg","mywindow","menubar=1,resizable=1,width=350,height=250");
+    }
